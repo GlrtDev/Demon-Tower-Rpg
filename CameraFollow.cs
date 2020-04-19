ed7d9a51fb5acbce2d83cc6092c3835cd334d5f7
GlrtDev <subin7777777@gmail.com>
2020-04-19 23:58:54 +0200

    Add CameraFollow script

---
+++ CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject followTarget;
+    private void Update()
+    {
+        iTween.MoveUpdate(gameObject, new Vector3(followTarget.transform.position.x, followTarget.transform.position.y,-10), 0.5f);
+    }
+}

062c494aea6ea07afa6db2c77d891fd2b0e0309f
GlrtDev <subin7777777@gmail.com>
2020-04-20 19:49:22 +0200

    Add abstract factory and abstract product

---
+++ Weapons/Abstracts/AbstractWeaponFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractWeaponFactory
+{
+    public abstract Sword CreateSword();
+    public abstract Magic CreateMagic();
+    public abstract Axe CreateAxe();
+    public abstract Dagger CreateDagger();
+}
---
+++ Weapons/Abstracts/AbstractWeaponProduct.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType {Sword, Axe , Dagger ,Magic }
+
+public abstract class AbstractWeaponProduct 
+{
+    public GameObject gameObject { get; protected set; } // Weapon GO made from prefab
+    public int damage { get; protected set; } // TODO make more stats , evan aray
+
+    public  void Instantiate(WeaponType weaponType)
+    {
+        gameObject = GameObject.
+    }
+
+    public void CombineWith(AbstractWeaponProduct other)
+    {
+        // prevent self interaction
+        if (other == this)
+            return;
+
+        if (other.gameObject == null)
+            return;
+
+        this.damage += (int)(0.1f * other.damage);
+        GameObject.Destroy(other.gameObject);
+        other = null;
+    }
+}

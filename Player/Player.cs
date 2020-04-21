9b1e755424f62e6c98649aa5f528e700fa30e79b
GlrtDev <subin7777777@gmail.com>
2020-04-22 01:25:51 +0200

    Add 3 player classes one main, one for movement ,other for attacks

---
+++ Player/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public WeaponClient weaponClient;
+    public AbstractWeaponProduct equipedWeapon;
+}
---
+++ Player/PlayerAttacks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour
+{
+    public Player player;
+    public Joystick joystick;
+
+    public Vector2 attackDir;
+    public Transform transform;
+
+    private void Start()
+    {
+        transform = gameObject.transform;
+    }
+
+    void Update()
+    {
+        attackDir.x = joystick.Horizontal;
+        attackDir.y = joystick.Vertical;
+        if(attackDir.sqrMagnitude != 0)
+        {
+            Vector3 currentPosition = transform.position;
+            
+            player.equipedWeapon.Attack(currentPosition ,attackDir);
+        }
+    }
+}

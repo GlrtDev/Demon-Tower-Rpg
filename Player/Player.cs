e3a6a4a0e65083456887f32d1b8ef249e1b870f3
GlrtDev <subin7777777@gmail.com>
2020-05-07 00:08:54 +0200

    Add more weapons on player list  to test

--- Player/Player.cs
+++ Player/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public WeaponClient weaponClient;
-    public AbstractWeaponProduct equipedWeapon;
+    
 
-    private void Start()
+    private void Awake()
     {
         weaponClient = new WeaponClient(new WeaponFactory());
-        weaponClient.CreateWeapon(WeaponType.Axe, 0, 0);
-        equipedWeapon = weaponClient.ReturnAllWeapons();
+        weaponClient.CreateWeapon(WeaponType.Dagger, 0, 0);
+        weaponClient.CreateWeapon(WeaponType.Dagger, 1, 5);
+        weaponClient.CreateWeapon(WeaponType.Sword, 0, 2);
+        weaponClient.CreateWeapon(WeaponType.Sword, 1, 6);
+        weaponClient.CreateWeapon(WeaponType.Sword, 1, 11);
+        weaponClient.CreateWeapon(WeaponType.Axe, 0, 2);
     }
 }

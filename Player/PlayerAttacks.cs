04c9ac7a2823e091e155495e98c5cde8c845cb4e
GlrtDev <subin7777777@gmail.com>
2020-05-07 00:09:27 +0200

    Attack method expand

--- Player/PlayerAttacks.cs
+++ Player/PlayerAttacks.cs
@@ -19,25 +19,28 @@ public class PlayerAttacks : MonoBehaviour
 
     void Update()
     {
-        attackDir.x = joystick.Horizontal;
-        attackDir.y = joystick.Vertical;
+        if (player.weaponClient.equipedWeapon != null) {
+            attackDir.x = joystick.Horizontal;
+            attackDir.y = joystick.Vertical;
 
-        if (((attackDir.sqrMagnitude > 0.9f) && Time.time >= nextAttackTime) || (attackDir.sqrMagnitude != 0.0f &&player.equipedWeapon.GetType() == typeof(Dagger)))
-        {
-            animator.SetBool("Attacking", true);
-            foreach (ParticleSystem particle in particleSystems)
-                particle.enableEmission = true;
-            Vector3 currentPosition = new Vector3(transform.position.x, transform.position.y + 2, 0);
+            if (((attackDir.sqrMagnitude > 0.9f) && Time.time >= nextAttackTime) || (attackDir.sqrMagnitude != 0.0f && player.weaponClient.equipedWeapon.GetType() == typeof(Dagger)))
+            {
+                animator.SetBool("Attacking", true);
+                foreach (ParticleSystem particle in particleSystems)
+                    particle.enableEmission = true;
+                Vector3 currentPosition = new Vector3(transform.position.x, transform.position.y + 2, 0);
 
-            //check attack speed stat
-            player.equipedWeapon.Attack(currentPosition, attackDir);
-            nextAttackTime = Time.time + 1f / player.equipedWeapon.GetAttackRate();
-        }
-        else if (attackDir.sqrMagnitude == 0)
-        {
-            animator.SetBool("Attacking", false);
-            foreach (ParticleSystem particle in particleSystems)
-                particle.enableEmission = false;
+                //check attack speed stat
+                player.weaponClient.equipedWeapon.Attack(currentPosition, attackDir);
+                if (player.weaponClient.equipedWeapon.GetType() != typeof(Dagger))
+                    nextAttackTime = Time.time + 1f / player.weaponClient.equipedWeapon.GetAttackRate(); //TO DO make it in function
+            }
+            else if (attackDir.sqrMagnitude == 0)
+            {
+                animator.SetBool("Attacking", false);
+                foreach (ParticleSystem particle in particleSystems)
+                    particle.enableEmission = false;
+            }
         }
     }
 }

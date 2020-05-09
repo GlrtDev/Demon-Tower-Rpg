4ef9da758388c66f48c440e2d9e7601516ffafe5
GlrtDev <subin7777777@gmail.com>
2020-05-10 01:06:19 +0200

    Add Grund crack class

---
+++ GroundCrack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCrack : MonoBehaviour
+{
+   
+    void Start()
+    {
+        iTween.ScaleTo(gameObject, iTween.Hash(
+            "delay", 1.0f,
+            "time", 1.0f,
+            "scale", Vector3.zero,
+            "easetype", iTween.EaseType.easeInQuint,
+            "oncomplete", "EndAnimation"));
+    }
+
+
+    void EndAnimation()
+    {
+        Destroy(gameObject);
+    }
+}

21639aadad9664b689f555e2bfee681e68ab9fde
GlrtDev <subin7777777@gmail.com>
2020-05-10 01:06:56 +0200

    Add UI controller class

---
+++ UiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    public GameObject inventory;
+
+    public void Start()
+    {
+        inventory.SetActive(true);
+        inventory.SetActive(false);
+    }
+
+    public void OpenInventory()
+    {
+        inventory.SetActive(!inventory.activeSelf);
+        if(inventory.activeSelf)
+        inventory.GetComponent<WeaponClientUI>().Refresh();
+    }
+}

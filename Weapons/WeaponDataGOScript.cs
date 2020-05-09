63daa8b1025e69aead75b6d7eb4b6dcac46fdf05
GlrtDev <subin7777777@gmail.com>
2020-05-10 01:07:31 +0200

    Add class to handle weapon GO values

---
+++ Weapons/WeaponDataGOScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDataGOScript : MonoBehaviour
+{
+    public Hashtable weaponHash;
+    private GameObject GroundCrackObject;
+
+    public void SpawnCrack()
+    {
+        GroundCrackObject = Resources.Load<GameObject>((string)weaponHash["CrackPath"]);
+        Debug.Log("spawn");
+        GameObject.Instantiate(GroundCrackObject, gameObject.transform.position, Quaternion.Euler(0,0,Random.Range(0,180)));
+    }
+}

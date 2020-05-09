14865cb4f627bf6d772788ceef3d1aa0a7687c17
GlrtDev <subin7777777@gmail.com>
2020-05-10 01:06:41 +0200

    Add spawn player point class

---
+++ SpawnPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForEndOfFrame();
+        player.position = gameObject.transform.position;
+    }
+}

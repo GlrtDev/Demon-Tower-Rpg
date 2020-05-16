08cee96847d7efab41d1dbf3de279c2d1f6764bd
GlrtDev <subin7777777@gmail.com>
2020-05-16 23:42:51 +0200

    Add delay to initial Astar scan

---
+++ AstarScanTiming.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstarScanTiming : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Scan");
+    }
+
+    IEnumerator Scan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        AstarPath.active.Scan();
+    }
+}
